cd8026120fba673e74e8988988196e7bf62f01f1
Unevilicorn <unevilicorn@gmail.com>
2022-11-12 18:00:11 +0000

    feat: dataset + feature selection
    Co-authored-by: Khemi <khemichew@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,38 +1,35 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import './App.css';
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import logo from "./logo.svg";
 import Flow from "./Visualisation";
+import Landing from "./Landing";
 
 function Home() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<Home/>}/>
-        <Route exact path="/visualisation" element={<Flow/>}/>
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/setup" element={<Landing />} />
+                <Route exact path="/visualisation" element={<Flow />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
