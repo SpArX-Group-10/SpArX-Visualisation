4b3ba51281a5240f783146adaa1e4426348415af
Siddharth Sharma <ss450555@gmail.com>
2022-11-12 16:32:16 +0000

    fix: show previous layer when click on node
    Co-authored-by: Dilshaan Singh Grewal <DilshaanGrewal@users.noreply.github.com>
    Co-authored-by: Ioana Cristina Mihailescu <IoanaCM@users.noreply.github.com>
    Co-authored-by: MIhneag <mihneaghitu2@gmail.com>

--- src/GraphElements.js
+++ src/GraphElements.js
@@ -9,7 +9,7 @@ export const nodes = [
       type: 'input',
       sourcePosition: Position.Right,
       targetPosition: Position.Left,
-      layer: '1',
+      layer: 1,
       hidden: true,
     },
     {
@@ -20,7 +20,7 @@ export const nodes = [
       type: 'input',
       sourcePosition: Position.Right,
       targetPosition: Position.Left,
-      layer: '1',
+      layer: 1,
       hidden: true,
     },
     {
@@ -31,7 +31,7 @@ export const nodes = [
       type: 'input',
       sourcePosition: Position.Right,
       targetPosition: Position.Left,
-      layer: '1',
+      layer: 1,
       hidden: true,
     },
     // Hidden Layer 1
@@ -42,7 +42,7 @@ export const nodes = [
       position: { x: 200, y: 0 },
       sourcePosition: Position.Right,
       targetPosition: Position.Left,
-      layer: '2',
+      layer: 2,
       hidden: true,
     },
     {
@@ -52,7 +52,7 @@ export const nodes = [
       position: { x: 200, y: 100 },
       sourcePosition: Position.Right,
       targetPosition: Position.Left,
-      layer: '2',
+      layer: 2,
       hidden: true,
     },
     {
@@ -62,7 +62,7 @@ export const nodes = [
       position: { x: 200, y: 200 },
       sourcePosition: Position.Right,
       targetPosition: Position.Left,
-      layer: '2',
+      layer: 2,
       hidden: true,
     },
     {
@@ -72,7 +72,7 @@ export const nodes = [
       position: { x: 200, y: 300 },
       sourcePosition: Position.Right,
       targetPosition: Position.Left,
-      layer: '2',
+      layer: 2,
       hidden: true,
     },
     // Hidden Layer 2
@@ -83,7 +83,7 @@ export const nodes = [
       position: { x: 400, y: 0 },
       sourcePosition: Position.Right,
       targetPosition: Position.Left,
-      layer: '3',
+      layer: 3,
       hidden: true,
     },
     {
@@ -93,7 +93,7 @@ export const nodes = [
       position: { x: 400, y: 100 },
       sourcePosition: Position.Right,
       targetPosition: Position.Left,
-      layer: '3',
+      layer: 3,
       hidden: true,
     },
     {
@@ -103,7 +103,7 @@ export const nodes = [
       position: { x: 400, y: 200 },
       sourcePosition: Position.Right,
       targetPosition: Position.Left,
-      layer: '3',
+      layer: 3,
       hidden: true,
     },
     {
@@ -113,7 +113,7 @@ export const nodes = [
       position: { x: 400, y: 300 },
       sourcePosition: Position.Right,
       targetPosition: Position.Left,
-      layer: '3',
+      layer: 3,
       hidden: true,
     },
     // Output Layer
@@ -125,7 +125,7 @@ export const nodes = [
       type: 'output',
       sourcePosition: Position.Right,
       targetPosition: Position.Left,
-      layer: '4'
+      layer: 4
     },
     {
       id: '13',
@@ -135,7 +135,7 @@ export const nodes = [
       type: 'output',
       sourcePosition: Position.Right,
       targetPosition: Position.Left,
-      layer: '4'
+      layer: 4
     },
     {
       id: '14',
@@ -145,29 +145,29 @@ export const nodes = [
       type: 'output',
       sourcePosition: Position.Right,
       targetPosition: Position.Left,
-      layer: '4'
+      layer: 4
     },
   ];
 
 export const edges = [
-  { id: '1-4', source: '1', target: '4', label: 'to the', type: 'straight', hidden: true, },
-  { id: '1-5', source: '1', target: '5', label: 'to the', type: 'straight', hidden: true, },
-  { id: '1-6', source: '1', target: '6', label: 'to the', type: 'straight', hidden: true, },
-  { id: '5-7', source: '5', target: '8', label: 'to the', type: 'straight', hidden: true, },
+  { id: '1-4', source: '1', target: '4', label: 'to the', type: 'straight', hidden: true, layer: 1 },
+  { id: '1-5', source: '1', target: '5', label: 'to the', type: 'straight', hidden: true, layer: 1 },
+  { id: '1-6', source: '1', target: '6', label: 'to the', type: 'straight', hidden: true, layer: 1 },
+  { id: '5-7', source: '5', target: '8', label: 'to the', type: 'straight', hidden: true, layer: 1 },
 
-  { id: '8-12', source: '8', target: '12', label: 'to the', type: 'straight', hidden: true, },
-  { id: '9-12', source: '9', target: '12', label: 'to the', type: 'straight', hidden: true, },
-  { id: '10-12', source: '10', target: '12', label: 'to the', type: 'straight', hidden: true, },
-  { id: '11-12', source: '11', target: '12', label: 'to the', type: 'straight', hidden: true, },
+  { id: '8-12', source: '8', target: '12', label: 'to the', type: 'straight', hidden: true, layer: 3 },
+  { id: '9-12', source: '9', target: '12', label: 'to the', type: 'straight', hidden: true, layer: 3 },
+  { id: '10-12', source: '10', target: '12', label: 'to the', type: 'straight', hidden: true, layer: 3 },
+  { id: '11-12', source: '11', target: '12', label: 'to the', type: 'straight', hidden: true, layer: 3 },
 
-  { id: '8-13', source: '8', target: '13', label: 'to the', type: 'straight', hidden: true, },
-  { id: '9-13', source: '9', target: '13', label: 'to the', type: 'straight', hidden: true, },
-  { id: '10-13', source: '10', target: '13', label: 'to the', type: 'straight', hidden: true, },
-  { id: '11-13', source: '11', target: '13', label: 'to the', type: 'straight', hidden: true, },
+  { id: '8-13', source: '8', target: '13', label: 'to the', type: 'straight', hidden: true, layer: 3 },
+  { id: '9-13', source: '9', target: '13', label: 'to the', type: 'straight', hidden: true, layer: 3 },
+  { id: '10-13', source: '10', target: '13', label: 'to the', type: 'straight', hidden: true, layer: 3 },
+  { id: '11-13', source: '11', target: '13', label: 'to the', type: 'straight', hidden: true, layer: 3 },
 
-  { id: '8-14', source: '8', target: '14', label: 'to the', type: 'straight', hidden: true, },
-  { id: '9-14', source: '9', target: '14', label: 'to the', type: 'straight', hidden: true, },
-  { id: '10-14', source: '10', target: '14', label: 'to the', type: 'straight', hidden: true, },
-  { id: '11-14', source: '11', target: '14', label: 'to the', type: 'straight', hidden: true, },
+  { id: '8-14', source: '8', target: '14', label: 'to the', type: 'straight', hidden: true, layer: 3 },
+  { id: '9-14', source: '9', target: '14', label: 'to the', type: 'straight', hidden: true, layer: 3 },
+  { id: '10-14', source: '10', target: '14', label: 'to the', type: 'straight', hidden: true, layer: 3 },
+  { id: '11-14', source: '11', target: '14', label: 'to the', type: 'straight', hidden: true, layer: 3 },
 ];
 
--- src/Visualisation.js
+++ src/Visualisation.js
@@ -1,8 +1,8 @@
-import ReactFlow, { Controls, Background, getIncomers, getConnectedEdges} from 'reactflow';
-import {useState} from 'react'
-import 'reactflow/dist/style.css';
-import { nodes as initialNodes, edges as initialEdges } from './GraphElements';
-import './GraphElementsStyle.css'
+import ReactFlow, { Controls, Background } from "reactflow";
+import { useState } from "react";
+import "reactflow/dist/style.css";
+import { nodes as initialNodes, edges as initialEdges } from "./GraphElements";
+import "./GraphElementsStyle.css";
 
 function Flow() {
   const [nodes, setNodes] = useState(initialNodes);
@@ -12,29 +12,25 @@ function Flow() {
     setNodes((nds) =>
       nds.map((node) => {
         if (node.layer === curNode.layer - 1) {
-          // when you update a simple type you can just update the value
           node.hidden = false;
         }
-
         return node;
       })
     );
-    // console.log(nodes);
-    // let incomingNodes = getIncomers(curNode, nodes, edges);
-    // let connectedEdges = getConnectedEdges([curNode], edges);
-    // incomingNodes.forEach(node => { node.hidden = false; });
-    // connectedEdges.forEach(edge => { edge.hidden = false; });
-    // setNodes(nodes);
-    // console.log(nodes);
-    // setEdges(edges);
-  }
+
+    setEdges((eds) =>
+      eds.map((edge) => {
+        if (edge.target === curNode.id) {
+          edge.hidden = false;
+        }
+        return edge;
+      })
+    );
+  };
 
   return (
-    <div style={{ height: '100vh' }}>
-      <ReactFlow 
-        nodes={nodes} 
-        edges={edges} 
-        onNodeClick={renderPreviousLayer}>
+    <div style={{ height: "100vh" }}>
+      <ReactFlow nodes={nodes} edges={edges} onNodeClick={renderPreviousLayer}>
         <Background />
         <Controls />
       </ReactFlow>
