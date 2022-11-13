e8ee9cb34e33eca83a52a4c1e8b8ada3aa871373
Siddharth Sharma <ss450555@gmail.com>
2022-11-13 14:06:29 +0000

    fix: bug with text size in hover tool

--- src/HoverCards.js
+++ src/HoverCards.js
@@ -11,15 +11,16 @@ export const NodeHoverCard = ({
   hoverPosition,
   zoom,
 }) => {
-  var incoming = "[" + incomingNodes.map(nd => nd.data.label).join(',') + "]";
+  var incoming = "[" + incomingNodes.map((nd) => nd.data.label).join(",") + "]";
 
   return (
     <div
       style={{
+        fontSize: `${(div_height * zoom) / 4}px`,
         width: div_width * zoom * 4,
         height: div_height * zoom,
         backgroundColor: "#F3F1F1",
-        border: '2px solid black',
+        border: "2px solid black",
         top: (node_height - div_height / 2) * zoom,
         left: (node_width * 2 + right_offset) * zoom,
         position: "absolute",
@@ -29,11 +30,9 @@ export const NodeHoverCard = ({
         }px, ${viewportPosition.Y + zoom * hoverPosition.Y}px)`,
       }}
     >
-        <text style={{ width: div_width * zoom * 4, height: div_height * zoom, fontSize: '1.2vmin', color: 'black' }}> 
-          Node argument: {argument} 
-          <br/> 
-          Directly supported/attacked by: {incoming} 
-        </text>
+      Node argument: {argument}
+      <br />
+      Directly supported/attacked by: {incoming}
     </div>
   );
 };
