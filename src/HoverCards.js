e4036e3881349314fb51856e78b86ae3f4c91994
Siddharth Sharma <ss450555@gmail.com>
2022-11-13 03:29:46 +0000

    fix: hover now properly works for nodes in network

--- src/HoverCards.js
+++ src/HoverCards.js
@@ -1,20 +1,64 @@
 import React from "react";
+const div_width = 50;
+const div_height = 50;
+const node_height = 30;
+const node_width = 30;
+const right_offset = 10;
+export const NodeHoverCard = ({
+  argument,
+  incomingNodes,
+  viewportPosition,
+  hoverPosition,
+  zoom,
+}) => {
+  return (
+    <div
+      style={{
+        width: div_width * zoom,
+        height: div_height * zoom,
+        backgroundColor: "red",
+        top: (node_height - div_height / 2) * zoom,
+        left: (node_width * 2 + right_offset) * zoom,
+        position: "absolute",
+        zIndex: 1,
+        transform: `translate(${
+          viewportPosition.X + zoom * hoverPosition.X
+        }px, ${viewportPosition.Y + zoom * hoverPosition.Y}px)`,
+      }}
+    >
+      <div style={{ width: 25, height: 25 }} className="argument">
+        {" "}
+        {argument}{" "}
+      </div>
+      <div style={{ width: 25, height: 25 }} className="incomingNodes">
+        {" "}
+        {incomingNodes}{" "}
+      </div>
+    </div>
+  );
+};
 
-export const NodeHoverCard = ({argument, incomingNodes}) => {
-    console.log("Got here")
-    return (
-        <div style={{width: 50, height: 50, backgroundColor: 'red'}}>
-            <div style={{width: 25, height: 25}} className = "argument"> {argument} </div>
-            <div style={{width: 25, height: 25}} className = "incomingNodes"> {incomingNodes} </div>
-        </div>
-    );
-}
-
-export const EdgeHoverCard = (props) => {
-    return (
-        <div style={{width: 50, height: 50, backgroundColor: 'red'}}>
-            <div style={{width: 25, height: 25}} className = "weight"> {props.weight} </div>
-            <div style={{width: 25, height: 25}} className = "edgeType"> {props.edgeType} </div>
-        </div>
-    );
-}
+export const EdgeHoverCard = ({ weight, edgeType, position }) => {
+  return (
+    <div
+      style={{
+        width: 50,
+        height: 50,
+        backgroundColor: "blue",
+        top: position.Y,
+        left: position.X,
+        position: "absolute",
+        zIndex: 100,
+      }}
+    >
+      <div style={{ width: 25, height: 25 }} className="weight">
+        {" "}
+        {weight}{" "}
+      </div>
+      <div style={{ width: 25, height: 25 }} className="edgeType">
+        {" "}
+        {edgeType}{" "}
+      </div>
+    </div>
+  );
+};
--- src/Visualisation.js
+++ src/Visualisation.js
@@ -1,5 +1,11 @@
-import ReactFlow, { Controls, Background, getIncomers } from "reactflow";
-import { useState } from "react";
+import ReactFlow, {
+  Controls,
+  Background,
+  getIncomers,
+  useStore,
+  ReactFlowProvider,
+} from "reactflow";
+import { useEffect, useState } from "react";
 import "reactflow/dist/style.css";
 import { nodes as initialNodes, edges as initialEdges } from "./GraphElements";
 import "./GraphElementsStyle.css";
@@ -7,8 +13,11 @@ import { NodeHoverCard, EdgeHoverCard } from "./HoverCards";
 
 const getInitialNodesHoverData = (nds, eds) => {
   var hoverStateCards = {};
-  nds.forEach(nd => {
-    hoverStateCards[nd.label] = { argument: nd.label, incomingNodes: getIncomers(nd, nds, eds), };
+  nds.forEach((nd) => {
+    hoverStateCards[nd.label] = {
+      argument: nd.label,
+      incomingNodes: getIncomers(nd, nds, eds),
+    };
   });
 
   return hoverStateCards;
@@ -16,21 +25,56 @@ const getInitialNodesHoverData = (nds, eds) => {
 
 const getInitialEdgesHoverData = (eds) => {
   var hoverStateCards = {};
-  eds.forEach(edge => {
-    hoverStateCards[edge.id] = { weight: 0, edgeType: 'SUPPORT', };
+  eds.forEach((edge) => {
+    hoverStateCards[edge.id] = { weight: 0, edgeType: "SUPPORT" };
   });
 
   return hoverStateCards;
 };
 
-function Flow() {
-  var initialNodesHoverData = getInitialNodesHoverData(initialNodes, initialEdges);
+const zoomSelector = (s) => s.transform[2];
+function Flow({
+  nodes,
+  edges,
+  renderPreviousLayer,
+  handleNodeEnter,
+  handleNodeLeave,
+  handleMoveEnd,
+  setZoom,
+}) {
+  const internal_zoom = useStore(zoomSelector);
+  useEffect(() => {
+    setZoom(internal_zoom);
+  }, [internal_zoom]);
+  return (
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodeClick={renderPreviousLayer}
+      onNodeMouseEnter={handleNodeEnter}
+      onNodeMouseLeave={handleNodeLeave}
+      onMoveEnd={handleMoveEnd}
+    >
+      <Background />
+      <Controls />
+    </ReactFlow>
+  );
+}
+
+function FlowWithProvider() {
+  var initialNodesHoverData = getInitialNodesHoverData(
+    initialNodes,
+    initialEdges
+  );
   var initialEdgesHoverData = getInitialEdgesHoverData(initialEdges);
 
   const [nodes, setNodes] = useState(initialNodes);
   const [edges, setEdges] = useState(initialEdges);
   const [isHovered, setIsHovered] = useState(false);
   const [hoveredNode, setHoveredNode] = useState(null);
+  const [viewportPosition, setViewportPosition] = useState({ X: 0, Y: 0 });
+  const [zoom, setZoom] = useState(1);
+  const [hoverPosition, setHoverPosition] = useState({ X: 0, Y: 0 });
 
   const renderPreviousLayer = (event, curNode) => {
     setNodes((nds) =>
@@ -52,18 +96,45 @@ function Flow() {
     );
   };
 
+  const handleNodeEnter = (event, node) => {
+    setIsHovered(true);
+    setHoveredNode(node);
+    setHoverPosition({ X: node.position.x, Y: node.position.y });
+  };
+
+  const handleNodeLeave = (event, node) => {
+    setIsHovered(false);
+    setHoveredNode(null);
+  };
+
+  const handleMoveEnd = (event, viewport) => {
+    setViewportPosition({ X: viewport.x, Y: viewport.y });
+    setZoom(viewport.zoom);
+  };
+
   return (
     <div style={{ height: "100vh" }}>
-      <ReactFlow nodes={nodes} edges={edges} 
-          onNodeClick={renderPreviousLayer}
-          onNodeMouseEnter={(event, node) => {setIsHovered(true); setHoveredNode(node)}}
-          onMouseLeave={(event, node) => {setIsHovered(false); setHoveredNode(null)}}>
-        {isHovered ? <NodeHoverCard data = {initialNodesHoverData[hoveredNode.label]}/> : null}
-        <Background />
-        <Controls />
-      </ReactFlow>
+      {isHovered ? (
+        <NodeHoverCard
+          data={initialNodesHoverData[hoveredNode.label]}
+          viewportPosition={viewportPosition}
+          hoverPosition={hoverPosition}
+          zoom={zoom}
+        />
+      ) : null}
+      <ReactFlowProvider>
+        <Flow
+          nodes={nodes}
+          edges={edges}
+          renderPreviousLayer={renderPreviousLayer}
+          handleNodeEnter={handleNodeEnter}
+          handleNodeLeave={handleNodeLeave}
+          handleMoveEnd={handleMoveEnd}
+          setZoom={setZoom}
+        />
+      </ReactFlowProvider>
     </div>
   );
 }
 
-export default Flow;
+export default FlowWithProvider;
