cec7d4ee748a04c04dc9024652ef87e04a05a012
Siddharth Sharma <ss450555@gmail.com>
2022-11-18 03:00:30 +0000

    feat: improve gui UI with Material UI

--- src/components/DatasetSelection.js
+++ src/components/DatasetSelection.js
@@ -1,62 +1,158 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Stack,
+  Typography,
+} from "@mui/material";
+import Papa from "papaparse";
+import React, { useEffect } from "react";
 import { useState } from "react";
-import CSVReader from "react-csv-reader";
 import Dataset from "../classes/Dataset";
+import UploadFileIcon from "@mui/icons-material/UploadFile";
+import NextButton from "./NextButton";
 
-function FeatureSelection({ headers, featureCallback }) {
-    let usableHeaders = headers.slice(0, -1);
+function FeatureSelection({
+  usableHeaders,
+  setSelectedFeatures,
+  selectedFeatures,
+}) {
+  useEffect(() => {}, []);
 
-    const [selectedFeatures, setSelectedFeatures] = useState(Array(usableHeaders.length).fill(true));
+  const handleCheckboxesChange = (i) => {
+    let newFeatures = selectedFeatures.slice();
+    newFeatures[i] = !newFeatures[i];
+    setSelectedFeatures(newFeatures);
+  };
 
-    const handleCheckboxesChange = (i) => {
-        let newFeatures = selectedFeatures.slice();
-        newFeatures[i] = !newFeatures[i];
-        setSelectedFeatures(newFeatures);
-    };
+  const checkboxes = usableHeaders.map((header, i) => (
+    <Box key={header + i} style={{}}>
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={selectedFeatures[i]}
+            id={header}
+            onChange={(event) => handleCheckboxesChange(i)}
+            sx={{
+              color: "#1a7d35",
+              "&.Mui-checked": {
+                color: "#1a7d35",
+              },
+            }}
+          />
+        }
+        label={header}
+        labelPlacement="end"
+      />
+    </Box>
+  ));
 
-    const checkboxes = usableHeaders.map((header, i) => (
-        <div key={header + i}>
-            <input
-                type="checkbox"
-                id={header}
-                checked={selectedFeatures[i]}
-                onChange={() => handleCheckboxesChange(i)}
-            />
-            <label>{header}</label>
-        </div>
-    ));
-
-    return (
-        <div>
-            {checkboxes}
-            <button onClick={() => featureCallback(selectedFeatures)}>Next</button>
-        </div>
-    );
+  return (
+    <Box
+      style={{
+        textAlign: "left",
+        justifyContent: "center",
+        alignItems: "center",
+        display: "flex",
+        marginTop: 20,
+      }}
+    >
+      <Stack>{checkboxes}</Stack>
+    </Box>
+  );
 }
 
 function DatasetSelection({ selectedDatasetCallback }) {
-    const [dataset, setDataset] = useState([]);
-    const [headers, setHeaders] = useState([]);
+  const [dataset, setDataset] = useState([]);
+  const [headers, setHeaders] = useState([]);
+  const [fileName, setFileName] = useState(null);
+  const [selectedFeatures, setSelectedFeatures] = useState(null);
 
-    const datasetLoaded = (data) => {
-        setDataset(data);
-        setHeaders(Object.keys(data[0]));
-    };
+  const datasetLoaded = (data) => {
+    setDataset(data);
+    setHeaders(Object.keys(data[0]));
+  };
 
-    const featureCallback = (features) => {
-        let datasetData = Dataset.constructFilteredData(dataset, headers, features);
-        selectedDatasetCallback(datasetData);
-    };
+  const featureCallback = (features) => {
+    let datasetData = Dataset.constructFilteredData(dataset, headers, features);
+    selectedDatasetCallback(datasetData);
+  };
 
-    return (
-        <div>
-            <CSVReader
+  const changeHandler = (event) => {
+    // Passing file data (event.target.files[0]) to parse using Papa.parse
+    setFileName(event.target.files[0].name);
+    Papa.parse(event.target.files[0], {
+      header: true,
+      skipEmptyLines: true,
+      dynamicTyping: true,
+      complete: function (results) {
+        setDataset(results.data);
+        let usableHeaders = Object.keys(results.data[0]).slice(0, -1);
+        setSelectedFeatures(Array(usableHeaders.length).fill(true));
+        setHeaders(Object.keys(results.data[0]));
+      },
+    });
+  };
+
+  return (
+    <Box style={{ height: "100%", width: "100%" }}>
+      <Box
+        style={{
+          justifyContent: "center",
+          alignItems: "start",
+          display: "flex",
+          height: "100%",
+          width: "100%",
+        }}
+      >
+        <Stack style={{ height: "100%", position: "relative" }}>
+          <Stack direction={"row"} style={{ marginTop: 15, marginRight: 15 }}>
+            {/* File Uploader */}
+            <Button
+              variant="contained"
+              component="label"
+              style={{ backgroundColor: "#1a7d35" }}
+            >
+              <UploadFileIcon />
+              Upload Dataset
+              <input
+                type="file"
+                name="file"
                 accept=".csv,.txt,.data"
-                parserOptions={{ header: true, dynamicTyping: true, skipEmptyLines: true }}
-                onFileLoaded={datasetLoaded}
+                onChange={changeHandler}
+                style={{ display: "none" }}
+              />
+            </Button>
+            <Box
+              style={{
+                border: "1px solid black",
+                borderRadius: "4px",
+                marginLeft: 15,
+                width: "300px",
+                justifyContent: "center",
+                alignItems: "center",
+                display: "flex",
+              }}
+            >
+              <Typography>{fileName}</Typography>
+            </Box>
+          </Stack>
+          <Typography style={{ marginTop: 35 }}>
+            Select desired features
+          </Typography>
+          {headers.length !== 0 && (
+            <FeatureSelection
+              usableHeaders={headers.slice(0, -1)}
+              setSelectedFeatures={setSelectedFeatures}
+              selectedFeatures={selectedFeatures}
             />
-            {headers.length !== 0 && <FeatureSelection headers={headers} featureCallback={featureCallback} />}
-        </div>
-    );
+          )}
+          <NextButton onClick={() => featureCallback(selectedFeatures)} />
+        </Stack>
+      </Box>
+    </Box>
+  );
 }
 
 export default DatasetSelection;
---
+++ src/components/NextButton.js
@@ -0,0 +1,32 @@
+import { Box, Button } from "@mui/material";
+import React from "react";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+
+const NextButton = ({ onClick }) => {
+  return (
+    <Box
+      style={{
+        position: "absolute",
+        bottom: "0px",
+        right: "0px",
+        marginBottom: 15,
+      }}
+    >
+      <Button
+        variant="contained"
+        style={{
+          borderRadius: "50%",
+          height: "65px",
+          width: "20px",
+          backgroundColor: "#1a7d35",
+          color: "white",
+        }}
+        onClick={onClick}
+      >
+        <ArrowForwardIcon />
+      </Button>
+    </Box>
+  );
+};
+
+export default NextButton;
