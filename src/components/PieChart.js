a2c187599c479bdc52723a30401ad5632bfd09cf
dsg20 <dsg20@ic.ac.uk>
2022-11-25 22:42:38 +0000

    feat: change pie chart label color

--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -1,32 +1,50 @@
 import { Pie } from "react-chartjs-2";
-import React, { useState } from "react";
+import { Chart, ChartOptions} from "chart.js";
+import React from "react";
 
 
 export const PieChartComponent = (supportingWeights, attackingWeights) => {
-  var labels = Object.keys(supportingWeights);
-  var data = Object.values(supportingWeights);
+  // var labels = Object.keys(supportingWeights);
+  // var data = Object.values(supportingWeights);
 
-  var colors = Array(labels.length).fill("#00FF00");
+  // var colors = Array(labels.length).fill("#00FF00");
+
+  // var datasets = [
+  //   {
+  //     data: data,
+  //     backgroundColor: colors,
+  //   }
+  // ]
+
+  // TODO: Fix labels and datasets for pie chart
+
+  var labels = ["2011", "2012", "2014", "2016", "2018"]
 
   var datasets = [
     {
-      data: data,
-      backgroundColor: colors,
+      data: [2000, 4000, 2300, 2222, 3333],
+      backgroundColor: ["#000000", "#58508d", "#bc5090", "#ff6361", "#ffa600"]
     }
   ]
 
   return (
     <div>
-      <Pie
-        options={{
-          width: "400",
-          height: "400",
-        }}
-        data={{
-          labels: labels,
-          datasets: datasets
-        }}
-      />
+        <Pie
+          options={{
+            width: "400",
+            height: "400",
+            legend: {
+              labels: {
+                fontColor: "white",
+              }
+            },
+          }}
+
+          data={{
+            labels: labels,
+            datasets: datasets
+          }}
+        />
     </div>
   );
 }
