3f8264edd83aaa52f09d705d7734be7fc76243ea
Unevilicorn <unevilicorn@gmail.com>
2022-11-30 14:22:10 +0000

    feat: separated gui + visualisation

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import logo from "./logo.svg";
 import Flow from "./Visualisation";
-import Landing from "./Landing";
-
-function Home() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
-}
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route exact path="/" element={<Landing />} />
-                <Route exact path="/visualisation" element={<Flow />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Flow />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
